d89e0c600e075721c8e5d4ac8a2d80ddd8e3bad6
Srivathsan <a.harni@gmail.com>
2020-06-17 17:02:40 -0500

    adding airflow exercise files.

---
+++ Data_Pipelines/exercises/dags/data_pipeline_exercise_1.py
@@ -0,0 +1,20 @@
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def greet():
+    logging.info("Hello World")
+
+dag = DAG(
+    'harini.demo1',
+    start_date=datetime.datetime.now()
+)
+
+greet_task = PythonOperator(
+    task_id = "greet_task",
+    python_callable = greet,
+    dag = dag
+    )
+
